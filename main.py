532a9bbf3418274c0af828a019f254e18f7d5a79
Dimitar Milkov <dimemilkov1@gmail.com>
2025-04-02 17:00:33 +0200

    agent setup

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import asyncio
-import os
+
 from dotenv import load_dotenv
-from livekit.agents import AutoSubscribe, JobContext, WorkerOptions, cli, llm  
+from livekit.agents import AutoSubscribe, JobContext, WorkerOptions, cli, llm
 from livekit.agents.voice_assistant import VoiceAssistant
 from livekit.plugins import openai, silero
+# from api import AssistantFnc
 
 load_dotenv()
+
 
 async def entrypoint(ctx: JobContext):
     initial_ctx = llm.ChatContext().append(
         role="system",
-        text=("You are a voice assistant created by LiveKit. Your interface with users will be voice."
-              "You should use short and concise responses, and avoiding usage of unpronouncable markdown.")
+        text=(
+            "You are a voice assistant created by LiveKit. Your interface with users will be voice. "
+            "You should use short and concise responses, and avoiding usage of unpronouncable punctuation."
+        ),
     )
     await ctx.connect(auto_subscribe=AutoSubscribe.AUDIO_ONLY)
+    # fnc_ctx = AssistantFnc()
 
     assistant = VoiceAssistant(
         vad=silero.VAD.load(),
@@ -21,24 +26,13 @@ async def entrypoint(ctx: JobContext):
         llm=openai.LLM(),
         tts=openai.TTS(),
         chat_ctx=initial_ctx,
+        # fnc_ctx=fnc_ctx,
     )
     assistant.start(ctx.room)
 
     await asyncio.sleep(1)
-    await assistant.say("Hello! How can I help you today?", allow_interruptions=True)
+    await assistant.say("Hey, how can I help you today!", allow_interruptions=True)
 
 
 if __name__ == "__main__":
     cli.run_app(WorkerOptions(entrypoint_fnc=entrypoint))
-    
-
-
-
-
-
-
-
-
-
-
-
